f3080e6d1f3f420d4b4a480f97d4c238e6198932
AGREXIO <riyaaman1819@Gmail.com>
2025-03-30 17:09:08 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,19 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  
   resolve: {
+    
     alias: {
       '/src/main.jsx': '/src/main.tsx', // Redirect old .jsx to new .tsx
+    },
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase', // Optional, agar CSS modules use kar rahe ho
     },
   },
 });
